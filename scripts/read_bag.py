ed3973d6af5105aa56c18c1a2559d8894a87cd86
Marina Aoki <marina.aoki@tum.de>
2025-01-17 17:20:51 +0900

    Add corrected sensor topics

--- scripts/read_bag.py
+++ scripts/read_bag.py
@@ -1,8 +1,16 @@
+#!/usr/bin/env python2
+
 import rosbag
 
-bag = rosbag.Bag('test.bag')
+from sensor_msgs.msg import Image
 
-for topic, msg, t in bag.read_messages(topics=['chatter', 'numbers']):
-    print(msg)
+bag = rosbag.Bag('/media/ubi-lab-desktop/Extreme Pro/kinect/data/Person099/B1_BED_OUT/image_raw.bag')
+
+for topic, msg, t in bag.read_messages():
+    print(topic)
+
+    print(type(msg))
+
+    print(msg.header)
 
 bag.close()
