fc10cd6707062b84e0dc51eb770ace53208622af
Marina Aoki <marina.aoki@tum.de>
2025-01-21 10:22:56 +0900

    Update data visualisation

--- scripts/read_bag.py
+++ scripts/read_bag.py
@@ -5,11 +5,14 @@ import rosbag
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 
-bag = rosbag.Bag('/media/ubi-lab-desktop/Extreme Pro/kinect/data/Person099/B1_BED_OUT/image_raw.bag')
+bag = rosbag.Bag('/media/ubi-lab-desktop/Extreme Pro/kinect/data/Person001/L2_FALL_DOWN/image_raw.bag')
 
 
 for topic, msg, stamp in bag.read_messages(topics=["depth/image_raw"]):
     cv_image = CvBridge().imgmsg_to_cv2(msg, desired_encoding="passthrough")
+    # normalise and then rescale to greyscale
+    cv_image = cv2.normalize(cv_image, None, 0, 255, cv2.NORM_MINMAX)
+    cv_image = cv2.convertScaleAbs(cv_image)
     cv2.imshow("Depth Image", cv_image)
     cv2.waitKey(1)
 
